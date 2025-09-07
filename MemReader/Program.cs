a6c6e860c6520a97da29a8d2c7acd23052b32074
felipebueno10 <felipebueno2005@hotmail.com>
2025-09-07 18:52:14 -0300

    Feat (Shared Memory): reader e writer were tested and are working

---
+++ MemReader/Program.cs
@@ -0,0 +1,92 @@
+﻿using System.IO.MemoryMappedFiles;
+
+class Reader
+{
+    static void Main()
+    {
+        // Declara variáveis para a memória e mutex incialmente nulas para realizar tentativas de abertura de objetos.
+        MemoryMappedFile mmf = null;
+        Mutex mutex = null;
+        int tentativas = 0;   // Contador de tentativas
+
+        // Laço para tentar abrir memória compartilhada, máximo de 50 tentativas.
+        while (mmf == null && tentativas < 50)
+        {
+            try { mmf = MemoryMappedFile.OpenExisting(MemIPC.NomeMem); } // Tenta abrir memória já existente
+            catch { Thread.Sleep(200); tentativas++; } // Se ainda não existir, espera 200 ms e incrementa contador de tentativas
+        }
+
+        // Se não abriu após 50 tentativas, encerra o programa com erro.
+        if (mmf == null) { Console.WriteLine("Erro ao abrir memória após 50 tentativas."); return; }
+
+        tentativas = 0; // Reinicia o contador de tenttivas
+
+        // Laço para tentar abrir o mutex, máximo de 50 tentativas.
+        while (mutex == null && tentativas < 50)
+        {
+            try { mutex = Mutex.OpenExisting(MemIPC.NomeMutex); } // Tenta abrir mutex já existente
+            catch { Thread.Sleep(200); tentativas++; } // Se ainda não existir, espera 200 ms e incrementa contador de tentativas
+        }
+
+        // Se não abriu após 50 tentativas, encerra o programa com erro.
+        if (mutex == null) { Console.WriteLine("Erro ao abrir mutex após 50 tentativas."); return; }
+
+        using (mmf)    // using (mmf) e using (mutex) garantem que, ao sair desse bloco,
+        using (mutex)  // os objetos serão liberados autoamticamente pelo C#
+        {
+            // Laço para leitura de mensagens. Termina quando o usuário digitar 'sair' no escritor e a ExitFlag se tornar 1.
+            while (true)
+            {
+                mutex.WaitOne();  // Aguarda o mutex ficar disponível
+
+                // Apenas var teria que chamar Dispose(), com using o C# garante que o recurso é limpo após uso.
+                using (var reader = mmf.CreateViewAccessor()) // Cria um view accessor para acessar a memória compartilhada
+                {
+                    byte exit = reader.ReadByte(MemIPC.ExitFlag); // Lê a flag de saída, na posição 512
+                    if (exit == 1)  // Se a flag = 1, o escritor sinalizou para encerrar
+                    {
+                        Console.WriteLine("Escritor finalizado. Encerrando leitor.");
+                        mutex.ReleaseMutex();  // Libera o mutexantes de sair
+                        break;
+                    }
+
+                    // Cria um buffer fixo de 256 chars.
+                    var buffer = new char[MemIPC.NumChars];
+
+                    // Copia array da memória pro buffer.
+                    reader.ReadArray(
+                        0,                // 
+                        buffer,           // 
+                        0,                // 
+                        MemIPC.NumChars   // 
+                        );
+
+                    var mensagem = new string(buffer).TrimEnd('\0'); // COnverte o buffer em string removendo '\0' do final
+
+                    // Só imprime se a mensagem não for nula ou vazia, evita que a mensagem seja lida e impressa em loop infinito.
+                    if (!string.IsNullOrEmpty(mensagem))
+                    {
+                        Console.WriteLine($"Mensagem lida: {mensagem}");
+
+                        // Limpa mensagem no buffer.
+                        Array.Clear(
+                            buffer,         // Array a ser limpo
+                            0,              // Posição inicial dentro do array
+                            buffer.Length   // Quantidade de char a limpar
+                            );
+
+                        // Limpa mensagem na memória compartilhada.
+                        reader.WriteArray(
+                            0,               // Posição inicial dentro da memória compartilhada
+                            buffer,          // O array que será gravado
+                            0,               // Posição inicial dentro do array
+                            MemIPC.NumChars  // Quantidade de chars que serão copiados
+                            );
+                    }
+                }
+                mutex.ReleaseMutex(); // Libera o mutex, permitindo que o escritor acesse a memória
+                Thread.Sleep(200); // Evita que o loop rode em alta velocidade ocupando 100 % da CPU quando não há mensagens novas
+            }
+        }
+    }
+}
---
+++ MemWriter/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.IO.MemoryMappedFiles;  // Fornece classes para trabalhar com memória compartilhada
+
+class Writer
+{
+    static void Main()
+    {
+        // Cria ou abre uma área de memória compartilhada no Windows. Se já existir apenas abre, se não, cria.
+        // Retorna um objeto MemoryMappedFile que internamente guarda um handle.
+        // using var: atalho pra criar o objeto e garantir que será liberado no final com Dispose()
+        using var mmf = MemoryMappedFile.CreateOrOpen(
+            MemIPC.NomeMem,     // Nome da área de memória
+            MemIPC.TamMemoria   // Tamanho da área de memória
+            );
+
+        // Cria ou abre um mutex. Se já existir apenas abre, se não, cria.
+        // Retorna um objeto Mutex que internamente guarda um handle.
+        // using var: atalho pra criar o objeto e garantir que será liberado no final com Dispose()
+        using var mutex = new Mutex(
+            false,             // Não começa com o mutex já bloqueado
+            MemIPC.NomeMutex   // Nome do mutex
+            );
+
+        Console.WriteLine("Iniciando escrita... \nDigite sua  mensagem e use 'sair' para encerrar.");
+
+        // Laço para escrita de mensagens. Termina quando o usuário digitar 'sair'.
+        while (true)
+        {
+            Console.Write("-> ");
+            string mensagem = Console.ReadLine() ?? "";
+
+            mutex.WaitOne(); // Trava o mutex e garante que apenas este processo irá acessar a memória.
+
+            // Apenas var teria que chamar Dispose(), com using o C# garante que o recurso é limpo após uso.
+            using (var writer = mmf.CreateViewAccessor()) // Cria um view accessor que ficará responsável por escrever na memória compartilhada.
+            {
+                if (string.Equals(mensagem, "sair", StringComparison.OrdinalIgnoreCase)) // Caso a mensagem digitada seja 'sair'
+                {
+                    writer.Write(MemIPC.ExitFlag, (byte)1); // Escreve 1 no byte da flag de saída na posição 512
+                    mutex.ReleaseMutex();                   // Libera o mutex, permitindo que o leitor acesse a memória
+                    break;
+                }
+
+                // Cria um buffer fixo de 256 chars.
+                var buffer = new char[MemIPC.NumChars];   // Cria array de char de tamanho 256
+                int n = Math.Min(                         // Calcula quantos caracteres vão ser copiados
+                    mensagem.Length,   // Se for menor que 256, copia todos
+                    MemIPC.NumChars    // Se for maior que 256, copia só os 256 primeiros
+                    );
+                mensagem.CopyTo(                          // Copia mensagem pro buffer
+                    0,                 // Começa no índice 0 da string original
+                    buffer,            // Copiar para variável buffer  
+                    0,                 // Começa a copiar desde o índice 0 do buffer
+                    n                  // Quantidade de caracteres a copiar
+                    );
+
+                // Escreve a mensagem na memória compartilhada e zera flag de saída.
+                writer.WriteArray(
+                    0,               // Posição inicial dentro da memória compartilhada
+                    buffer,          // O array que será gravado
+                    0,               // Posição inicial dentro do array
+                    MemIPC.NumChars  // Quantidade de chars que serão copiados
+                    );
+                writer.Write(
+                    MemIPC.ExitFlag, // Posição na memória compartilhada onde gravar
+                    (byte)0          // Valor a ser escrito, aqui um byte de valor 0
+                    );
+
+                Console.WriteLine($"Mensagem escrita: {mensagem}");
+            }
+            mutex.ReleaseMutex(); // Libera o mutex, permitindo que o leitor acesse a memória
+        }
+    }
+}
