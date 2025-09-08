339e5fa0ea406305de38aa859a9d910c0a6f6186
viniciuspjr <viniciuspelissari.2004@gmail.com>
2025-09-08 14:26:53 -0300

    feat(SocketServer): Add server

---
+++ SocketClient/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SocketClient
+{
+    class Program
+    {
+        static void JsonLog(string evento, string detalhe = "")
+        {
+            Console.WriteLine("{ \"event\": \"" + evento + "\", \"detail\": \"" + detalhe + "\" }");
+        }
+
+        static void Main(string[] args)
+        {
+            int port = 12345;
+            string ip = "127.0.0.1";
+
+            TcpClient client = null;
+            NetworkStream stream = null;
+
+            try
+            {
+                client = new TcpClient();
+                client.Connect(IPAddress.Parse(ip), port);
+                JsonLog("client_started");
+                JsonLog("connected_to_server", "127.0.0.1:12345");
+
+                stream = client.GetStream();
+                byte[] buffer = new byte[512];
+
+                Console.WriteLine("Digite uma mensagem para enviar ao servidor (ou 'sair'):");
+                string msg;
+                while ((msg = Console.ReadLine()) != null)
+                {
+                    if (msg == "sair") break;
+
+                    byte[] msgBytes = Encoding.UTF8.GetBytes(msg);
+                    stream.Write(msgBytes, 0, msgBytes.Length);
+                    JsonLog("message_sent", msg);
+
+                    int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                    if (bytesRead > 0)
+                    {
+                        string resposta = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                        JsonLog("message_received", resposta);
+                    }
+                    else
+                    {
+                        JsonLog("server_disconnected");
+                        break;
+                    }
+                    Console.WriteLine("Digite uma mensagem para enviar ao servidor (ou 'sair'):");
+                }
+            }
+            catch (Exception ex)
+            {
+                JsonLog("error", ex.Message);
+            }
+            finally
+            {
+                if (stream != null)
+                    stream.Close();
+                if (client != null)
+                    client.Close();
+                JsonLog("client_shutdown");
+            }
+        }
+    }
+}
---
+++ Sockets/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SocketServer
+{
+    class Program
+    {
+        static void JsonLog(string evento, string detalhe = "")
+        {
+            Console.WriteLine("{ \"event\": \"" + evento + "\", \"detail\": \"" + detalhe + "\" }");
+        }
+
+        static void Main(string[] args)
+        {
+            int port = 12345;
+            TcpListener servidor = null;
+            TcpClient cliente = null;
+
+            try
+            {
+                servidor = new TcpListener(IPAddress.Parse("127.0.0.1"), port);
+                servidor.Start();
+                JsonLog("server_started", "Socket criado");
+                JsonLog("bind_success", "127.0.0.1:12345");
+                JsonLog("waiting_connection");
+
+                cliente = servidor.AcceptTcpClient();
+                JsonLog("client_connected");
+
+                NetworkStream stream = cliente.GetStream();
+                byte[] buffer = new byte[512];
+
+                int bytesRead;
+                while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    string mensagem = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    JsonLog("message_received", mensagem);
+
+                    string reply = "Eco: " + mensagem;
+                    byte[] replyBytes = Encoding.UTF8.GetBytes(reply);
+                    stream.Write(replyBytes, 0, replyBytes.Length);
+                    JsonLog("message_sent", reply);
+                }
+
+                JsonLog("client_disconnected");
+                stream.Close();
+                cliente.Close();
+            }
+            catch (Exception ex)
+            {
+                JsonLog("error", ex.Message);
+            }
+            finally
+            {
+                if (servidor != null)
+                    servidor.Stop();
+                JsonLog("server_shutdown");
+            }
+        }
+    }
+}
